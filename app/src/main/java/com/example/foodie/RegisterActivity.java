e938a3522e532e0a88ced3019b29f79e996cd45d
bejiVIII <simone0443@outlook.com>
2024-05-04 17:00:17 +0300

    add image, small ui changes

--- app/src/main/java/com/example/foodie/RegisterActivity.java
+++ app/src/main/java/com/example/foodie/RegisterActivity.java
@@ -3,24 +3,18 @@ package com.example.foodie;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     Button btn;
     EditText usernameText;
@@ -29,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.register_activity);
 
         btn = findViewById(R.id.sendButton);
         usernameText = findViewById(R.id.usernameText);
@@ -48,10 +42,10 @@ public class MainActivity extends AppCompatActivity {
                     response -> {
                         // Handle response
 
-                        Log.d("Response: ", response);
+                        Log.d("Main Activity", response);
                     }, error -> {
                 // Handle errors
-                Log.e("Error", error.toString());
+                Log.e("Main Activity", error.toString());
             }) {
                 @Override
                 protected Map<String, String> getParams() {
