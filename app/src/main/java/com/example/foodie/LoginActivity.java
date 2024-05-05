229089c63d6ef2f5268bac7e4d522fb6bb404d3a
bejiVIII <simone0443@outlook.com>
2024-05-05 17:43:47 +0300

    login system implemented

--- app/src/main/java/com/example/foodie/LoginActivity.java
+++ app/src/main/java/com/example/foodie/LoginActivity.java
@@ -8,6 +8,17 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     //TODO: login php
@@ -17,9 +28,6 @@ public class LoginActivity extends AppCompatActivity {
     Button btn;
     EditText usernameText;
     EditText passwordText;
-
-    TextView alreadyAccText;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,69 @@ public class LoginActivity extends AppCompatActivity {
         passwordText = findViewById(R.id.passwordText);
 
         newAccountText = findViewById(R.id.noAccText);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = BuildConfig.BASE_URL + "/foodie/Login.php";
+
+        btn.setOnClickListener(v -> {
+            final String name = String.valueOf(usernameText.getText());
+            final String password = String.valueOf(passwordText.getText());
+            // Request a string response from the provided URL.
+            Log.d("Login", String.format("Username: %s\nPassword: %s", name, password));
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    response -> {
+                        // Handle response
+
+                        Log.d("Login", "Response: " + response);
+                        System.out.println("Response: " + response);
+                        try {
+                            JSONObject json = new JSONObject(response);
+                            String responseValue = json.getString("response");
+                            switch(responseValue)
+                            {
+                                case "Success": {
+                                    Intent i = new Intent(v.getContext(), RestaurantsActivity.class);
+                                    i.putExtra("username", name);
+                                    startActivity(i);
+                                    Log.d("Login", "Success");
+                                    break;
+                                }
+                                case "Fail": {
+                                    Log.d("Login", "Already exists");
+                                    Toast.makeText(v.getContext(), "Wrong username or password. Try again.", Toast.LENGTH_SHORT).show();
+
+                                    break;
+                                }
+                                default: {
+                                    Toast.makeText(v.getContext(), "An error occurred.", Toast.LENGTH_SHORT).show();
+
+                                    break;
+                                }
+                            }
+
+                        } catch (Exception e) {
+                            Log.e("Login", String.format("%s: %s", e, e.getMessage()));
+                        }
+                    }, error -> {
+                // Handle errors
+                Log.e("Login", String.format("%s: %s", error, error.getMessage()));
+            }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("username", name);
+                    params.put("password", password);
+                    return params;
+                }
+            };
+            queue.add(stringRequest);
+
+            System.out.println("Name: " + name);
+            System.out.println("Password: " + password);
+            System.out.println("url" + url);
+
+
+        });
 
         newAccountText.setOnClickListener(v -> {
             Intent i = new Intent(v.getContext(), RegisterActivity.class);
