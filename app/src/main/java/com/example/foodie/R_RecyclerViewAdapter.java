eb2d67aff944abdb056c6c0b4d616045fe067d78
bejiVIII <simone0443@outlook.com>
2024-05-06 22:15:14 +0300

    add restaurants list, 'Picasso' dependency for better image rendering, small changes

---
+++ app/src/main/java/com/example/foodie/R_RecyclerViewAdapter.java
@@ -0,0 +1,77 @@
+package com.example.foodie;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+
+public class R_RecyclerViewAdapter extends RecyclerView.Adapter<R_RecyclerViewAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<RestaurantModel> restaurantModels;
+
+    public R_RecyclerViewAdapter(Context context, ArrayList<RestaurantModel> restaurantModels) {
+        this.context = context;
+        this.restaurantModels = restaurantModels;
+    }
+
+    //onCreateViewHolder -> for giving the layout 'restaurants_recycler_view_row' to each row
+    @NonNull
+    @Override
+    public R_RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("RecyclerAdapter", "OnCreateViewHolder");
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.restaurants_recycler_view_row, parent, false);
+        return new R_RecyclerViewAdapter.MyViewHolder(view);
+    }
+
+    //onBindViewHolder -> we give the data from 'restaurantModels' to each row
+    @Override
+    public void onBindViewHolder(@NonNull R_RecyclerViewAdapter.MyViewHolder holder, int position) {
+        Log.d("RecyclerAdapter", "OnBindViewHolder");
+        //binding the data to the ViewHolder
+        holder.restaurantName.setText(restaurantModels.get(position).getName());
+        holder.restaurantDescription.setText(restaurantModels.get(position).getDescription());
+
+
+        Picasso.get().load(restaurantModels.get(position).getImage()).fit().into(holder.restaurantImage);
+
+        //holder.restaurantImage.setImageResource(restaurantModels.get(position).getImage());
+    }
+
+    //getItemCount -> for the adapter to know how many items we have in the list
+    @Override
+    public int getItemCount() {
+        return restaurantModels.size();
+    }
+
+    //MyViewHolder -> looks like onCreate method, everytime we create a view holder
+    //in 'onCreateViewHolder' we provide the card's components id to the viewholder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView restaurantImage;
+        TextView restaurantName, restaurantDescription;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            restaurantImage = itemView.findViewById(R.id.restaurantImageView);
+            restaurantName = itemView.findViewById(R.id.restaurantNameTextView);
+            restaurantDescription = itemView.findViewById(R.id.restaurantDescriptionTextView);
+
+
+        }
+    }
+}
--- app/src/main/java/com/example/foodie/RestaurantsActivity.java
+++ app/src/main/java/com/example/foodie/RestaurantsActivity.java
@@ -1,22 +1,55 @@
 package com.example.foodie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class RestaurantsActivity extends AppCompatActivity {
 
     TextView welcomeTextView;
+
+    ArrayList<RestaurantModel> restaurantModels = new ArrayList<>();
+
+    RecyclerView restRecyclerView;
+    int[] restaurantImages = {R.drawable.restaurant1, R.drawable.restaurant2, R.drawable.restaurant3,
+            R.drawable.restaurant1, R.drawable.restaurant2, R.drawable.restaurant3,
+            R.drawable.restaurant1, R.drawable.restaurant2, R.drawable.restaurant3,
+            R.drawable.restaurant1, R.drawable.restaurant2, R.drawable.restaurant3,
+            R.drawable.restaurant1, R.drawable.restaurant2, R.drawable.restaurant3,
+            R.drawable.restaurant1, R.drawable.restaurant2, R.drawable.restaurant3,
+            R.drawable.restaurant1, R.drawable.restaurant2, R.drawable.restaurant3};
+    ListView restListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
 
-        Intent i = getIntent();
-        String username = i.getStringExtra("username");
+        String username = getIntent().getStringExtra("username");
         welcomeTextView = findViewById(R.id.welcomeText);
         welcomeTextView.setText(String.format("Welcome, %s", username));
+
+        restRecyclerView = findViewById(R.id.restRecyclerView);
+
+        setUpRestaurantModels();
+
+        restRecyclerView.setAdapter(new R_RecyclerViewAdapter(this, restaurantModels));
+        restRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void setUpRestaurantModels() {
+        String[] restaurantNames = getResources().getStringArray(R.array.restaurants);
+        String[] descriptions = getResources().getStringArray(R.array.descriptions);
+
+        for(int i = 0; i < restaurantNames.length; i++ ) {
+            restaurantModels.add(new RestaurantModel(restaurantNames[i], descriptions[i], restaurantImages[i]));
+        }
     }
 }
